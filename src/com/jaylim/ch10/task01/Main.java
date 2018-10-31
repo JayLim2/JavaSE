dea61789ea0cd57d2b2fbb8d4c7aa197c04ea100
JayLim <ddx180874@gmail.com>
2018-11-01 01:04:23 +0400

    Resolved chapter 10 task 7 and partly resolved task 1

--- src/com/jaylim/ch10/task01/Main.java
+++ src/com/jaylim/ch10/task01/Main.java
@@ -1,6 +1,8 @@
 package com.jaylim.ch10.task01;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,24 +14,32 @@ import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        //fixme unresolved
+    public static void main(String[] args) throws IOException {
+        //fixme unresolved "Как найти только первый файл?"
+        List<Path> files = getFilesByWord("D:\\testDir", "aba");
+        for (Path file : files) {
+            System.out.println(file.toString());
+        }
     }
 
     public static List<Path> getFilesByWord(String pathName, String word) throws IOException {
         Path path = Paths.get(pathName);
         ArrayList<Path> files = new ArrayList<>();
-        try(Stream<Path> pathStream = Files.walk(path)) {
+        try (Stream<Path> pathStream = Files.walk(path).parallel()) {
             pathStream.forEach(new Consumer<Path>() {
                 @Override
                 public void accept(Path path) {
-                    if(path.toFile().isFile()) {
-                        Scanner scanner = new Scanner(System.in);
-                        while (scanner.hasNext()) {
-                            if(scanner.next().equals(word)) {
-                                files.add(path);
-                                return;
+                    File file = path.toFile();
+                    if (file.isFile()) {
+                        try (Scanner scanner = new Scanner(new FileReader(file))) {
+                            while (scanner.hasNext()) {
+                                if (scanner.next().equals(word)) {
+                                    files.add(path);
+                                    return;
+                                }
                             }
+                        } catch (FileNotFoundException ex) {
+                            System.out.println(ex.getMessage());
                         }
                     }
                 }
--- src/com/jaylim/ch10/task07/Main.java
+++ src/com/jaylim/ch10/task07/Main.java
@@ -1,11 +1,32 @@
 package com.jaylim.ch10.task07;
 
+import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Main {
     private static ConcurrentHashMap<String, Long> map = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
-        // FIXME: 31.10.2018 unresolved
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            map.putIfAbsent("element " + i, random.nextLong());
+        }
+
+        for (Map.Entry<String, Long> entry : map.entrySet()) {
+            System.out.println(entry);
+        }
+
+        Map.Entry<String, Long> entry = map.reduceEntries(5, new BiFunction<Map.Entry<String, Long>, Map.Entry<String, Long>, Map.Entry<String, Long>>() {
+            @Override
+            public Map.Entry<String, Long> apply(Map.Entry<String, Long> stringLongEntry, Map.Entry<String, Long> stringLongEntry2) {
+                return Long.compare(stringLongEntry.getValue(), stringLongEntry2.getValue()) > 0 ? stringLongEntry : stringLongEntry2;
+            }
+        });
+
+        System.out.println("\nmax entry:");
+        System.out.println(entry);
     }
 }
